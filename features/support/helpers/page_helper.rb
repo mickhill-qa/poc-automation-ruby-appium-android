b0dc6c8280e5358f3718da55a3bdc9d5650d0e20
Mick Hill - MacBook Pro M1 <mickhill@hotmail.com.br>
2022-02-14 14:35:42 -0300

    Implementando testes

--- features/support/helpers/page_helper.rb
+++ features/support/helpers/page_helper.rb
@@ -3,8 +3,14 @@ Dir[File.join(File.dirname(__FILE__), '../pages/*.rb')].each { |file| require fi
 
 # Depois que pegar os arquivos com (_page.rb) estancio todos as suas variaveis
 module PageHelper
-  def page_home
-    @page_home = GoogleHome.new
+  def page_inicial
+    @page_inicial = InicialPage.new
+  end
+  def page_login_options
+    @page_login_options = LoginOptionsPage.new
+  end
+  def page_login_email
+    @page_login_email = LoginEmailPage.new
   end
 end
 
